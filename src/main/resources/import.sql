7b6c3eeeb10796721061c7b43dff25436f623b09
Wpnnt <93552279+Wpnnt@users.noreply.github.com>
2023-11-09 15:09:03 -0300

    Delete src/main/resources/import.sql

--- src/main/resources/import.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO usuario(nome, senha, email) VALUES ('Joao', '123456', 'joao@gmail.com');
-INSERT INTO usuario(nome, senha, email) VALUES ('Maria', 'senha123', 'maria@yahoo.com');
-INSERT INTO usuario(nome, senha, email) VALUES ('Fulano', 'minha_senha', 'fulano@hotmail.com');
